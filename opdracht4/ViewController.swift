8f28c95a47f0e6bc5543323bedea87b44734ef48
student <student@mc10902.local>
2018-10-17 10:44:18 +0200

    Initial Commit

---
+++ opdracht4/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  opdracht4
+//
+//  Created by student on 11/10/2018.
+//  Copyright © 2018 Vives. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var hetis_WORD: [UILabel]!
+    
+    @IBOutlet var vijf_MIN: [UILabel]!
+    
+    
+    @IBOutlet var tien_MIN: [UILabel]!
+    
+    @IBOutlet var kwart: [UILabel]!
+
+    @IBOutlet var over: [UILabel]!
+    
+    @IBOutlet var voor: [UILabel]!
+    
+    @IBOutlet var half: [UILabel]!
+    
+    //uren
+    @IBOutlet var een_UUR: [UILabel]!
+    @IBOutlet var twee_UUR: [UILabel]!
+    @IBOutlet var drie_UUR: [UILabel]!
+    @IBOutlet var vier_UUR: [UILabel]!
+    @IBOutlet var vijf_UUR: [UILabel]!
+    @IBOutlet var zes_UUR: [UILabel]!
+    @IBOutlet var zeven_UUR: [UILabel]!
+    @IBOutlet var acht_UUR: [UILabel]!
+    @IBOutlet var negen_UUR: [UILabel]!
+    @IBOutlet var tien_UUR: [UILabel]!
+    @IBOutlet var elf_UUR: [UILabel]!
+    @IBOutlet var twaalf_UUR: [UILabel]!
+    
+    func updateTime(){
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
